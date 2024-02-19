eba79bae701518f2f3680bf5238ddedb7f1f21ba
Michael <tvq3bn@virginia.edu>
2024-02-19 10:55:58 -0500

    final commit

--- gettimings.c
+++ gettimings.c
@@ -1,93 +1,133 @@
 #define _XOPEN_SOURCE 700
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
 #include <signal.h>
-#include <string.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
-long long currtime;
-long long totaltime;
-long long globaloverhead;
-struct timespec tstart, tend;
+long long current_time;
+long long total_time;
+long long global_overhead;
+struct timespec time_start, time_end;
 
+pid_t other_pid1;
 pid_t other_pid2;
 
-// Function Prototypes
-void emptyFunction(void);
-long long measureempty(void);
-void getppidCall(void);
-long long measureppid(void);
-void systemCall(void);
-long long measurebintrue(void);
-void handler_usr1(int sig);
-long long measurecurrsig(void);
-long long measurecrosssig(void);
-void measurecrosssigneg1(void);
+__attribute__((noinline)) void empty_function(void) {
+    __asm__("");
+    return;
+}
 
-// Empty function for scenario 1
-void emptyFunction(void) {
+long long measure_empty(void) {
+    struct timespec start, end;
+    int repetition = 100;
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for(int i = 0; i < repetition; i++) {
+
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    long long overhead = (end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for (int i = 0; i < repetition; i++) {
+        empty_function();
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    long long execution = (end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);
+
+    long long total_ns = execution - overhead;
+
+    return total_ns / repetition;
+}
+
+long long measure_parent_pid(void) {
+    struct timespec start, end;
+    int repetition = 100;
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for(int i = 0; i < repetition; i++) {
+
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    long long overhead = (end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for (int i = 0; i < repetition; i++) {
+        getppid();
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    long long execution = (end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);
+
+    long long total_ns = execution - overhead;
+
+    return total_ns / repetition;
+}
+
+long long measure_bin_true(void) {
+    struct timespec start, end;
+    int repetition = 100;
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for(int i = 0; i < repetition; i++) {
+
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    long long overhead = (end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for (int i = 0; i < repetition; i++) {
+        system("/usr/bin/true");
+    }
+    clock_gettime(CLOCK_MONOTONIC, &end);
+    long long execution = (end.tv_sec - start.tv_sec) * 1000000000 + (end.tv_nsec - start.tv_nsec);
+
+    long long total_ns = execution - overhead;
+
+    return total_ns / repetition;
+}
+
+__attribute__((noinline)) void handle_signal_usr1(int id) {
+    clock_gettime(CLOCK_MONOTONIC, &time_end);
+    long long execution = (time_end.tv_sec - time_start.tv_sec) * 1000000000 + (time_end.tv_nsec - time_start.tv_nsec);
+    current_time = execution - global_overhead;
+    total_time += current_time;
     __asm__("");
 }
 
-// Measure time taken by emptyFunction
-long long measureempty(void) {
-    // Implementation similar to what you've provided
-    return 0; // Placeholder return
-}
+long long measure_current_signal(void) {
+    int repetition = 100;
 
-// Call getppid for scenario 2
-void getppidCall(void) {
-    (void)getppid();
-}
+    clock_gettime(CLOCK_MONOTONIC, &time_start);
 
-// Measure time taken by getppidCall
-long long measureppid(void) {
-    // Implementation similar to what you've provided
-    return 0; // Placeholder return
-}
+    clock_gettime(CLOCK_MONOTONIC, &time_end);
+    global_overhead = (time_end.tv_sec - time_start.tv_sec) * 1000000000 + (time_end.tv_nsec - time_start.tv_nsec);
 
-// Make a system call for scenario 3
-void systemCall(void) {
-    system("/usr/bin/true");
-}
+    struct sigaction act;
+    act.sa_handler = &handle_signal_usr1;
+    sigaction(SIGUSR1, &act, NULL);
 
-// Measure time taken by systemCall
-long long measurebintrue(void) {
-    // Implementation similar to what you've provided
-    return 0; // Placeholder return
-}
-
-// Signal handler for scenario 4 and 5
-void handler_usr1(int sig) {
-    clock_gettime(CLOCK_MONOTONIC, &tend);
-    currtime = (tend.tv_sec - tstart.tv_sec) * 1000000000 + (tend.tv_nsec - tstart.tv_nsec) - globaloverhead;
-    totaltime += currtime;
-}
-
-// Measure time for current process signal handling
-long long measurecurrsig(void) {
-    // Implementation similar to what you've provided
-    return 0; // Placeholder return
-}
-
-// Special listener function for -1 argument
-void measurecrosssigneg1(void) {
-    printf("Listener process PID: %d\n", getpid());
-    printf("Waiting for SIGUSR1...\n");
-
-    struct sigaction sa = {0};
-    sa.sa_handler = &handler_usr1;
-    sigaction(SIGUSR1, &sa, NULL);
-
-    // Infinite loop to keep the process alive
-    while(1) {
-        pause(); // Wait for signals
+    for (int i = 0; i < repetition; i++) {
+        clock_gettime(CLOCK_MONOTONIC, &time_start);
+        raise(SIGUSR1); 
+        usleep(10000);  
     }
+
+    return total_time / repetition;
 }
 
-// Measure time for inter-process communication via signals
-long long measurecrosssig(void) {
+long long measure_cross_signal(void) {
+    int repetition = 100;
+
+    clock_gettime(CLOCK_MONOTONIC, &time_start);
+
+    clock_gettime(CLOCK_MONOTONIC, &time_end);
+    global_overhead = (time_end.tv_sec - time_start.tv_sec) * 1000000000 + (time_end.tv_nsec - time_start.tv_nsec);
+
     printf("PID: %d\n", getpid());
     printf("Enter the PID of the other process: ");
     scanf("%d", &other_pid2);
@@ -97,54 +137,70 @@ long long measurecrosssig(void) {
     sigaddset(&set, SIGUSR1);
     sigprocmask(SIG_BLOCK, &set, NULL);
 
-    struct sigaction sa = {0};
-    sa.sa_handler = &handler_usr1;
-    sigaction(SIGUSR1, &sa, NULL);
-
-    for (int i = 0; i < 100; i++) {
-        clock_gettime(CLOCK_MONOTONIC, &tstart);
+    for (int i = 0; i < repetition; i++) {
+        clock_gettime(CLOCK_MONOTONIC, &time_start);
         kill(other_pid2, SIGUSR2);
         int signo;
-        sigwait(&set, &signo);
-        clock_gettime(CLOCK_MONOTONIC, &tend);
-        long long time_elapsed = (tend.tv_sec - tstart.tv_sec) * 1000000000 + (tend.tv_nsec - tstart.tv_nsec);
-        totaltime += time_elapsed - globaloverhead;
-        usleep(10000);
+        if(sigwait(&set, &signo) == 0){
+            if(signo == SIGUSR1){
+                clock_gettime(CLOCK_MONOTONIC, &time_end);
+                long long execution = (time_end.tv_sec - time_start.tv_sec) * 1000000000 + (time_end.tv_nsec - time_start.tv_nsec);
+                current_time = execution - global_overhead;
+                total_time += current_time;
+            }
+        }
+        usleep(10000);  
     }
-    return totaltime / 100;
+
+    return total_time / repetition;
 }
 
+void handle_signal_usr2_negative1(int id) {
+    kill(other_pid1, SIGUSR1);
+    __asm__("");
+}
+
+long long measure_cross_signal_negative1(void) {
+    struct sigaction act;
+    act.sa_handler = &handle_signal_usr1; 
+    sigaction(SIGUSR1, &act, NULL);  
+
+    act.sa_handler = &handle_signal_usr2_negative1;
+    sigaction(SIGUSR2, &act, NULL);  
+
+    printf("PID: %d\n", getpid());
+    printf("Enter the PID of the other process: ");
+    scanf("%d", &other_pid1);
+
+    while(1) {
+        sleep(1);
+    }    
+}
+
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <operation_number>\n", argv[0]);
-        exit(EXIT_FAILURE);
+    if (atoi(argv[1]) == 1) {
+        long long time = measure_empty();
+        printf("%lli\n", time);
     }
-
-    if (strcmp(argv[1], "-1") == 0) {
-        measurecrosssigneg1(); // Handling -1 case
-        return 0;
+    if (atoi(argv[1]) == 2) {
+        long long time = measure_parent_pid();;
+        printf("%lli\n", time);
     }
-
-    int operation = atoi(argv[1]);
-    switch (operation) {
-        case 1:
-            printf("Average time: %lld ns\n", measureempty());
-            break;
-        case 2:
-            printf("Average time: %lld ns\n", measureppid());
-            break;
-        case 3:
-            printf("Average time: %lld ns\n", measurebintrue());
-            break;
-        case 4:
-            printf("Average time: %lld ns\n", measurecurrsig());
-            break;
-        case 5:
-            printf("Average time: %lld ns\n", measurecrosssig());
-            break;
-        default:
-            fprintf(stderr, "Invalid operation number.\n");
-            exit(EXIT_FAILURE);
+    if (atoi(argv[1]) == 3) {
+        long long time = measure_bin_true();
+        printf("%lli\n", time);
+    }
+    if (atoi(argv[1]) == 4) {
+        long long time = measure_current_signal();
+        printf("%lli\n", time);
+    }
+    if (atoi(argv[1]) == 5) {
+        long long time = measure_cross_signal();
+        printf("%lli\n", time);
+    }
+    if (atoi(argv[1]) == -1) {
+        measure_cross_signal_negative1();
     }
 
     return 0;
